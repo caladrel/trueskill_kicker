779d2e27996e226554f8e8cbf4962fdbc925ef48
Marius Lewerenz <marius.lewerenz@gmail.com>
2014-03-05 14:37:33 +0100

    Fetch data for trueskill from scraper.
    Also show rank for player as µ-3*σ and sort players by that.

--- test_trueskill.py
+++ test_trueskill.py
@@ -1,30 +1,30 @@
 from trueskill import Rating, rate
-import json
+import scraper
 
 
 class test:
     def __init__(self):
         self.locker_room = {}
 
-        json_data = open("data.json")
+        _, results = scraper.scrape_matches(history=True)
 
-        self.data = json.load(json_data)
-        json_data.close()
-
-        for match in self.data:
-            self.process_match(match)
-
+        for result in reversed(results):
+            line = []
+            line.extend(result[0].split(' / '))
+            line.extend(result[1].split(' / '))
+            line.extend(result[2].split(':'))
+            self.process_match(line)
 
     def process_match(self, line):
         players = map(str, line[:4])
         score = map(int, line[4:])
         locker_room = self.locker_room
         for player in players:
-            if not locker_room.has_key(player):
+            if player not in locker_room:
                 locker_room[player] = Rating()
         t1 = [locker_room.pop(players[i]) for i in xrange(2)]
         t2 = [locker_room.pop(players[i]) for i in xrange(2, 4)]
-        ranks = [0, 0] if score[1] == score[0] else [0, 1] if score[0] > score[1] else [1, 0]
+        ranks = [score[1] > score[0], score[1] < score[0]]
         t1, t2 = rate([t1, t2], ranks=ranks)
         for i in xrange(2):
             locker_room[players[i]] = t1[i]
@@ -32,5 +32,7 @@ class test:
 
 
 tr = test()
-result = [[k, v.mu -3*v.sigma] for k, v in tr.locker_room.iteritems()]
-print result
+i = tr.locker_room.iteritems()
+
+for score, player in reversed(sorted((v.mu - 3 * v.sigma, k) for k, v in i)):
+    print player, score

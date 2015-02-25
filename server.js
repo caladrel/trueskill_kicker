a552088ca72fc12cbe498c676d58c48d65cf0cb9
Marius Lewerenz <marius.lewerenz@gmail.com>
2015-02-25 19:43:25 +0100

    improvements for the matchmaking

--- server.js
+++ server.js
@@ -16,55 +16,83 @@ var state = {
    players: []
 }
 
+function broadcastcount (socket) {
+    if ('available' in io.sockets.adapter.rooms)
+        count = Object.keys(io.sockets.adapter.rooms['available']).length;
+    else
+        count = 0;
+    socket.emit('availablecount', count);
+    socket.broadcast.emit('availablecount', count);
+
+    enable = state.checking || count > 3
+
+    socket.emit('enable', enable);
+    socket.broadcast.emit('enable', enable);
+}
+
+function broadcastready (socket) {
+    socket.emit('ready', state.players);
+    socket.broadcast.to('ready').emit('ready', state.players);
+}
+
+
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('available', function (data) {
         socket.join('available');
         console.log(socket.id, 'player', data, 'is available');
+        broadcastcount(socket);
     });
     socket.on('unavailable', function (data) {
         socket.leave('available');
         console.log(socket.id, 'player', data, 'is not available anymore');
+        broadcastcount(socket);
     });
     socket.on('checkready', function (data) {
-        //console.log(io.sockets.adapter.rooms['available']);
         socket.leave('available');
         socket.join('ready');
         if (!state.checking) {
             state.checking = true;
-            state.players = [];
+            state.players = {};
             socket.broadcast.to('available').emit('check', data);
         }
-        if (state.players.length < 4) {
-            state.players.push(data);
-            socket.emit('ready', state.players);
-            socket.broadcast.to('ready').emit('ready', state.players);
+        if (Object.keys(state.players).length < 4) {
+            state.players[socket.id] = data;
+            broadcastready(socket);
         }
-        if (state.players.length == 4) {
+        if (Object.keys(state.players).length == 4) {
             state.checking = false;
             socket.emit('finished');
             socket.broadcast.to('ready').emit('finished');
         }
         console.log(socket.id, 'player', data, 'is ready');
         console.log(state.players);
+        broadcastcount(socket);
     });
     socket.on('ready', function (data) {
         if (state.checking) {
             socket.leave('available');
             socket.join('ready');
-            if (state.players.length < 4) {
-                state.players.push(data);
-                socket.emit('ready', state.players);
-                socket.broadcast.to('ready').emit('ready', state.players);
+            if (Object.keys(state.players).length < 4) {
+                state.players[socket.id] = data;
+                broadcastready(socket);
             }
-            if (state.players.length == 4) {
+            if (Object.keys(state.players).length == 4) {
                 state.checking = false;
                 socket.emit('finished', state.players);
                 socket.broadcast.to('ready').emit('finished');
             }
             console.log(socket.id, 'player', data, 'is ready');
             console.log(state.players);
+            broadcastcount(socket);
         }
+    });
+    socket.on('disconnect', function () {
+        if (socket.id in state.players) {
+            delete state.players[socket.id];
+            broadcastready(socket);
+        }
+        broadcastcount(socket);
     });
 });
 

f945396dc53479203328618e5d57597a90dcc6fc
Marius Lewerenz <marius.lewerenz@gmail.com>
2015-02-27 12:40:47 +0100

    Replace confirm() with a bootstrap modal.

--- server.js
+++ server.js
@@ -16,83 +16,79 @@ var state = {
    players: []
 }
 
-function broadcastcount (socket) {
+function broadcastcount () {
     if ('available' in io.sockets.adapter.rooms)
         count = Object.keys(io.sockets.adapter.rooms['available']).length;
     else
         count = 0;
-    socket.emit('availablecount', count);
-    socket.broadcast.emit('availablecount', count);
+
+    io.emit('availablecount', count);
 
     enable = state.checking || count > 3
 
-    socket.emit('enable', enable);
-    socket.broadcast.emit('enable', enable);
+    io.emit('enable', enable);
 }
 
-function broadcastready (socket) {
-    socket.emit('ready', state.players);
-    socket.broadcast.to('ready').emit('ready', state.players);
+function broadcastready () {
+    io.sockets.in('ready').emit('ready', state.players);
 }
 
+function finish () {
+    state.checking = false;
+    io.sockets.in('ready').emit('finished');
+    if ('ready' in io.sockets.adapter.rooms)
+        for (id in io.sockets.adapter.rooms['ready'])
+            io.sockets.connected[id].leave('ready');
+}
+
+function ready (id, player) {
+    if (Object.keys(state.players).length < 4) {
+        state.players[id] = player;
+        broadcastready();
+    }
+    if (Object.keys(state.players).length == 4)
+        finish();
+
+    console.log(id, 'player', player, 'is ready');
+    console.log(state.players);
+    broadcastcount();
+}
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
+    broadcastcount()
+
     socket.on('available', function (data) {
         socket.join('available');
         console.log(socket.id, 'player', data, 'is available');
-        broadcastcount(socket);
+        broadcastcount();
     });
     socket.on('unavailable', function (data) {
         socket.leave('available');
         console.log(socket.id, 'player', data, 'is not available anymore');
-        broadcastcount(socket);
+        broadcastcount();
     });
     socket.on('checkready', function (data) {
-        socket.leave('available');
-        socket.join('ready');
+        socket.leave('available').join('ready');
         if (!state.checking) {
             state.checking = true;
             state.players = {};
-            socket.broadcast.to('available').emit('check', data);
+            io.sockets.in('available').emit('check', data);
         }
-        if (Object.keys(state.players).length < 4) {
-            state.players[socket.id] = data;
-            broadcastready(socket);
-        }
-        if (Object.keys(state.players).length == 4) {
-            state.checking = false;
-            socket.emit('finished');
-            socket.broadcast.to('ready').emit('finished');
-        }
-        console.log(socket.id, 'player', data, 'is ready');
-        console.log(state.players);
-        broadcastcount(socket);
+        ready(socket.id, data);
     });
     socket.on('ready', function (data) {
+        console.log('got ready');
         if (state.checking) {
-            socket.leave('available');
-            socket.join('ready');
-            if (Object.keys(state.players).length < 4) {
-                state.players[socket.id] = data;
-                broadcastready(socket);
-            }
-            if (Object.keys(state.players).length == 4) {
-                state.checking = false;
-                socket.emit('finished', state.players);
-                socket.broadcast.to('ready').emit('finished');
-            }
-            console.log(socket.id, 'player', data, 'is ready');
-            console.log(state.players);
-            broadcastcount(socket);
+            socket.leave('available').join('ready');
+            ready(socket.id, data);
         }
     });
     socket.on('disconnect', function () {
         if (socket.id in state.players) {
             delete state.players[socket.id];
-            broadcastready(socket);
+            broadcastready();
         }
-        broadcastcount(socket);
+        broadcastcount();
     });
 });
 
